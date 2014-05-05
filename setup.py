b2a3ac210ad51074f39085930dd94109e7a4b65e
Matt ODonnell <matt.odonnell@leandog.com>
2014-05-05 18:30:25 -0400

    moved things into structure easier to submit to pypi

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='MattyMocks',
+      version='0.1',
+      description='Yet another mocking library',
+      author='Matt ODonnell',
+      author_email='matty@mavenrd.com',
+      url='https://github.com/mattyo/mattysmocks',
+      packages=['mattysmocks', 'mattysmocks.tests', 'mattysmocks.tests.mock_these'],
+     )
