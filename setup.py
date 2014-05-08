0d1f99897c3998bc9db43a36fff2a557e06cbfe2
Matt ODonnell <matt.odonnell@leandog.com>
2014-05-08 17:17:21 -0400

    moved files to inner directory

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from distutils.core import setup
 
-setup(name='MattyMocks',
+setup(name='mattys_mocks',
       version='0.1',
       description='Yet another mocking library',
       author='Matt ODonnell',
       author_email='matty@mavenrd.com',
       url='https://github.com/mattyo/mattysmocks',
-      packages=['mattysmocks', 'mattysmocks.tests', 'mattysmocks.tests.mock_these'],
+      packages=['mattys_mocks', 'mattys_mocks.tests', 'mattys_mocks.tests.mock_these'],
      )
